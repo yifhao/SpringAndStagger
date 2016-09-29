52d0e5a151acc434d89c22dd632fb763528defae
haoyifen <1124210681@qq.com>
2016-09-29 16:10:23 +0800

    进一步精简

--- src/main/java/com/swagger/doc/MySwaggerConfig.java
+++ src/main/java/com/swagger/doc/MySwaggerConfig.java
@@ -14,23 +14,9 @@ public class MySwaggerConfig {
     // @Bean
     // public Docket restApi() {
     //     return new Docket(DocumentationType.SWAGGER_2)
-    //             // .groupName("")
-    //             .apiInfo(apiInfo())
-    //             // .groupName("")
-    //             // .securitySchemes(asList(
-    //             //         new OAuth(
-    //             //                 "petstore_auth",
-    //             //                 asList(new AuthorizationScope("write_pets", "modify pets in your account"),
-    //             //                         new AuthorizationScope("read_pets", "read your pets")),
-    //             //                 Arrays.<GrantType>asList(new ImplicitGrant(new LoginEndpoint("http://petstore.swagger.io/api/oauth/dialog"), "tokenName"))
-    //             //         ),
-    //             //         new ApiKey("api_key", "api_key", "header")
-    //             // ))
     //             .select()
     //             .paths(Predicates.and(ant("/**"), Predicates.not(ant("/error")), Predicates.not(ant("/management/**")), Predicates.not(ant("/management*"))))
-    //             .build()
-    //             .pathMapping("/");
-    //             // .pathProvider(new BasePathAwareRelativePathProvider("/"));
+    //             .build();
     // }
     //
     //
--- src/main/java/com/swagger/doc/controller/UserController.java
+++ src/main/java/com/swagger/doc/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.swagger.doc.controller;
 
 import com.swagger.doc.po.User;
-import com.swagger.doc.service.UserService;
 import com.swagger.doc.vo.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,44 +16,17 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Created by haoyifen on 16-9-28 2016 下午5:47
  */
-@Api(value = "user")
+@Api(value = "User控制器")
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    UserService userService;
-
-    // @ResponseBody
-    // @RequestMapping(value = "addUser", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-    // @ApiOperation(value = "添加新用户", httpMethod = "POST", response = Result.class, notes = "add new user")
-    // public String addUser(@ApiParam(required = true, name = "postData", value = "用户信息json数据") @RequestParam(
-    //         value = "postData") String postData, HttpServletRequest request) {
-    //     if (null == postData || postData.isEmpty()) {
-    //         return super.buildFailedResultInfo(-1, "post data is empty!");
-    //     }
-    //
-    //     UserInfo user = JSON.parseObject(postData, UserInfo.class);
-    //     int result = userService.addUser(user);
-    //     return buildSuccessResultInfo(result);
-    // }
-
-    // @ResponseBody
-    // @RequestMapping(
-    //         value = "queryUserById", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    // @ApiOperation(value = "根据用户ID查询用户信息")
-    // public String queryUserById(@ApiParam(required = true, name = "userId", value = "user id") @RequestParam(
-    //         value = "userId") String userId, HttpServletRequest request) {
-    //     UserInfo info = userService.queryUserById(Integer.parseInt(userId));
-    //
-    //     return buildSuccessResultInfo(info);
-    // }
-
+    @ApiOperation(value = "根据用户id查询用户信息", httpMethod = "GET", produces = "application/json")
+    @ApiResponse(code = 200, message = "success", response = Result.class)
     @ResponseBody
     @RequestMapping(value = "queryUserById", method = RequestMethod.GET, produces = "application/json")
-    @ApiOperation(value = "根据用户id查询用户信息", httpMethod = "GET", produces = "application/json")
-    public Result queryUserById(@ApiParam(required = true, name = "userId") @RequestParam("userId") int userId, HttpServletRequest request) {
-        User user = userService.queryUserById(userId);
+    public Result queryUserById(@ApiParam(name = "userId", required = true, value = "用户Id") @RequestParam("userId") int userId, HttpServletRequest request) {
+        User user = new User(userId, "haoyifen", 24);
         Result result = new Result();
         result.setCode(0);
         result.setData(user);
